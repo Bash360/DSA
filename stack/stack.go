d18fa4c0ddb881c24ecb7a8381d1dddf2208dcdb
bash360 <mark.bashir.ugwi@gmail.com>
2025-12-04 22:45:22 +0100

    data should not be visible implementation should be encapulsated

--- stack/stack.go
+++ stack/stack.go
@@ -1,16 +1,16 @@
 package stack
 
 type Stack[T any] struct {
-	Data []T
+	data []T
 	Size int
 }
 
 func New[T any](args ...T) *Stack[T] {
-	return &Stack[T]{Data: args, Size: len(args)}
+	return &Stack[T]{data: args, Size: len(args)}
 }
 func (self *Stack[T]) Push(val T) {
-	self.Data = append(self.Data, val)
-	self.Size = len(self.Data)
+	self.data = append(self.data, val)
+	self.Size = len(self.data)
 
 }
 
@@ -19,10 +19,10 @@ func (self *Stack[T]) Pop() T {
 		panic("empty stack")
 	}
 
-	result := self.Data[self.Size-1]
+	result := self.data[self.Size-1]
 
-	self.Data = self.Data[:self.Size-1]
-	self.Size = len(self.Data)
+	self.data = self.data[:self.Size-1]
+	self.Size = len(self.data)
 	return result
 
 }
@@ -32,9 +32,9 @@ func (self *Stack[T]) Peek() T {
 		panic("empty stack")
 
 	}
-	return self.Data[self.Size-1]
+	return self.data[self.Size-1]
 }
 
 func (self *Stack[T]) IsEmpty() bool {
-	return len(self.Data) == 0
+	return len(self.data) == 0
 }
